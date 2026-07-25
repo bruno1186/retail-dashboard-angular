82ea257a99ecc15e8aecdf78f5fbaa4f4785cf11
Bruno <84094962+bruno1186@users.noreply.github.com>
2026-07-25 16:01:07 -0300

    Create karma.conf.js

---
+++ karma.conf.js
@@ -0,0 +1,24 @@
+// Karma configuration para execucao headless em CI.
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma'),
+    ],
+    reporters: ['progress'],
+    browsers: ['ChromeHeadless'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu'],
+      },
+    },
+    singleRun: true,
+    restartOnFileChange: false,
+  });
+};
